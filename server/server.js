903c4f110c57d3f685b2fb61fade162b3a04395f
Alejandra <alejandra.e.bernabel@gmail.com>
2023-11-29 14:18:19 -0500

    Began adding session cookies using express-session

--- server/server.js
+++ server/server.js
@@ -5,9 +5,33 @@ const express = require('express');
 const session = require('express-session');
 const db = require ('./config/connection');
 const routes = require ('./routes');
+const MongoStore = require('connect-mongo');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+const sess = {
+  secret: process.env.SESSIONSECRET,
+  cookie: {
+    maxAge: 600000 *5, //cookie expires in 5 minutes of inactivity
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+  },
+  store: MongoStore.create({ 
+    client: mongoose.connection.getClient(),
+    dbName: process.env.MONGO_DB_NAME,
+    collectionName: "sessions",
+    stringify: false,
+    autoRemove: "interval",
+    autoRemoveInterval: 1
+  })
+
+}
+
+app.use (session(sess));
+
 
 app.use (express.urlencoded({ extended: true }));
 app.use(express.json());
