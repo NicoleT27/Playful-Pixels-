af2fe325511cb301766f47fee33c23ad28be7c9d
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-11-29 13:40:45 -0500

    Mads: Added files and images. Ale:initial server

---
+++ server/routes/index.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const router = express.Router();
+const apiRouter = require('./api');
+const path = require('path');
+
+//API routes
+router.use('/api', apiRouter);
+
+//connects server to our client folder
+router.use(express.static(path.join(__dirname, '../../client/dist')));
+
+//all other routes not accounted for will take the user to index.html (which should be our lobby, probably)
+router.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname), '../../client/dist/index.html');
+});
+
+module.exports = router;
