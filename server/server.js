f498edb50f70ef54f5279192c92290fbf4e76054
Alejandra <alejandra.e.bernabel@gmail.com>
2023-11-29 13:22:14 -0500

    Began working on server.js setup

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const db = require ('./config/connection');
+const routes = require ('./controllers');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use (express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Now listening on port ${PORT}`);
+    });
+  });
