d1da06182cf3d849dfde0db15cd5358f4df1a2c3
Nicole Torres <nicolejoan9@yahoo.com>
2023-12-04 15:44:51 -0500

    Nicole-trying to fix Vite HMR

--- client/src/pages/candy.jsx
+++ client/src/pages/candy.jsx
@@ -1,32 +1,31 @@
-// const width = 8
-// const candyColors=[
-//     'blue',
-//     'green',
-//     'orange',
-//     'purple',
-//     'red',
-//     'yellow'
-// ]
+import {useState} from 'react'
+const width = 8
+const candyColors=[
+    'blue',
+    'green',
+    'orange',
+    'purple',
+    'red',
+    'yellow'
+]
 
-const App = () => {
-    // const randomColorArrangement = []
-    // const createBoard = () => {
-    //     for (let i = 0; i < width * width; i++) {
-    //     const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
-    //     randomColorArrangement.push(randomColor)  
-    //     }
-    //     console.log(randomColorArrangement);
-    // }
-    // createBoard()
-    return(
-        <div>This is my temporary candy page.</div>
-    )
-}
 
-function candyCrush() {
-  return <div><h2>Welcome to Candy Crush</h2></div>;
+
+const candyCrush = () => {
+ const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+    
+    const createBoard = () => {
+        const randomColorArrangement = []
+        for (let i = 0; i < width * width; i++) {
+        const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+        randomColorArrangement.push(randomColor)  
+        }
+        // console.log(randomColorArrangement);
+    }
+    createBoard()
+    return <div>Welcome to Candy Crush</div>;
 }
 
 
 export default candyCrush;
-// export default App;
+
