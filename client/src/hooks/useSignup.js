c3e021461249ab61fb662b692acaa5c02f80d29c
Alejandra <alejandra.e.bernabel@gmail.com>
2023-12-04 23:27:05 -0500

    fixed user-routes function bug

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -4,7 +4,7 @@ import { useAuthContext } from "./useAuthContext";
 export const useSignup = () => {
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(null);
-    const { dispatch } = useAuthContext;
+    const { dispatch } = useAuthContext();
 
     const signup = async(username, email, password) => {
         setIsLoading(true);
