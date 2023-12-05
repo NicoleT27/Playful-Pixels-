cffac68c26e67c6b437d7ffa56a4a7f5547b91f0
Alejandra <alejandra.e.bernabel@gmail.com>
2023-12-04 23:31:48 -0500

    User token is now saved on signup. Still need to work on displaying errors on repeated email or username

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useReducer } from "react";
 export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
-    switch (action) {
+    switch (action.type) {
         case 'LOGIN':
             return { user: action.payload };
 
