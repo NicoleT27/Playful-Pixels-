0112eb1dd5e85558065624debcb80d1b4753b75e
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-12-07 20:56:41 -0500

    fixed image pathing and added cards

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,10 +1,13 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './home.css'
+import machine from '../assets/images/Arcade machine-amico.png';
 
 function home() {
   return (
+    <div className='d-inline-flex'>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="../assets/images/Arcade machine-amico.png" />
+      <Card.Img variant="top" src= {machine} />
       <Card.Body>
         <Card.Title>Candy Crush</Card.Title>
         <Card.Text>
@@ -13,6 +16,31 @@ function home() {
         <Button variant="primary">Go To Game</Button>
       </Card.Body>
     </Card>
+
+{/* second card  */}
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src= {machine} />
+      <Card.Body>
+        <Card.Title>Memory Game</Card.Title>
+        <Card.Text>
+          
+        </Card.Text>
+        <Button variant="primary">Go To Game</Button>
+      </Card.Body>
+    </Card>
+
+{/* third card  */}
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src= {machine} />
+      <Card.Body>
+        <Card.Title>Tic Tac Toe</Card.Title>
+        <Card.Text>
+          
+        </Card.Text>
+        <Button variant="primary">Go To Game</Button>
+      </Card.Body>
+    </Card>
+    </div>
   );
 }
 
