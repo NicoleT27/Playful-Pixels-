1a36e3fb74b62fc871a0f7e98a45cdb9753e074a
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-12-10 19:09:03 -0500

    added responsiveness to header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import './header.css'
-import { Link } from 'react-router-dom'
-import NavBar from '../Navigation/Navbar'
-const header = () => {
-  return (
-    <div>
-  <h1>Playful Pixels</h1>
-    <NavBar />
-</div>
+// import React from 'react'
+// import './header.css'
+// import { Link } from 'react-router-dom'
+// import NavBar from '../Navigation/Navbar'
+// const header = () => {
+//   return (
+//     <div>
+//       <h1>Playful Pixels</h1>
+//       <NavBar />
+//     </div>
 
-  )
+//   )
+// }
+
+// export default header
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from '../Navigation/Navbar';
+import './header.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function header() {
+  return (
+    <>
+      <Container>
+        <Row className='mb-0' >
+          <Col className="pb-0">
+            <h1 className='PP' >
+              <Link to="/">
+              Playful Pixels
+              </Link>
+              </h1>
+          </Col>
+        </Row>
+      </Container>
+      <NavBar />
+    </>
+  );
 }
 
-export default header
+export default header;
