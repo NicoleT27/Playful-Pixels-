da4f6d9f87ac0c9883ec3e6291c57bfe5d6c0fed
Alejandra <alejandra.e.bernabel@gmail.com>
2023-12-11 11:20:12 -0500

    Iproved tic tac toe styling:

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -3,6 +3,7 @@ import Square from "./Square";
 import { calculateWinner } from "../utils/helpers";
 import "../components/Board.css"
 
+
 function Board() {
 
     const [squares, setSquares] = React.useState(Array(9).fill(null));
@@ -39,19 +40,19 @@ function Board() {
     return (
         <div className="board">
             <div className="ticTacToeRow">
-                <Square value={squares[0]} onClick = {() => handleClick(0)}/>
-                <Square value={squares[1]} onClick = {() => handleClick(1)}/>
-                <Square value={squares[2]} onClick = {() => handleClick(2)} />
+                <div className="boxDecoration right"><Square value={squares[0]} onClick = {() => handleClick(0)} /></div>
+                <div className="boxDecoration right"><Square value={squares[1]} onClick = {() => handleClick(1)}/></div>
+                <div className="boxDecoration"><Square value={squares[2]} onClick = {() => handleClick(2)}/></div>
             </div>
             <div className="ticTacToeRow">
-                <Square value={squares[3]} onClick = {() => handleClick(3)} />
-                <Square value={squares[4]} onClick = {() => handleClick(4)}/>
-                <Square value={squares[5]} onClick = {() => handleClick(5)}/>
+                <div className="boxDecoration right top"><Square value={squares[3]} onClick = {() => handleClick(3)} /></div>
+                <div className="boxDecoration right top"><Square value={squares[4]} onClick = {() => handleClick(4)}/></div>
+                <div className="boxDecoration top"><Square value={squares[5]} onClick = {() => handleClick(5)}/></div>
             </div>
             <div className="ticTacToeRow">
-                <Square value={squares[6]} onClick = {() => handleClick(6)}/>
-                <Square value={squares[7]} onClick = {() => handleClick(7)}/>
-                <Square value={squares[8]} onClick = {() => handleClick(8)}/>
+                <div className="boxDecoration right top"><Square value={squares[6]} onClick = {() => handleClick(6)} /></div>
+                <div className="boxDecoration right top"><Square value={squares[7]} onClick = {() => handleClick(7)}/></div>
+                <div className="boxDecoration top"><Square value={squares[8]} onClick = {() => handleClick(8)}/></div>
             </div>
             <div className="status">{status}</div>
             <button className="restart" onClick={handleRestart}>Restart Game</button>
