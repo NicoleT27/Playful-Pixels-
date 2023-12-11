2d8415a0ad15e2b5e01348c5ad99f6b8c3b253b8
Alejandra <alejandra.e.bernabel@gmail.com>
2023-12-11 10:10:47 -0500

    Began working on tic tac toe styling

--- client/src/components/Navigation/Navbar.jsx
+++ client/src/components/Navigation/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useLogout } from "../../hooks/useLogout";
 import { useAuthContext } from "../../hooks/useAuthContext";
-import
+import machine from '../../assets/images/Arcade machine-amico.png';
 
 
 // Alejandra made this nav bar. Feel free to style it. It was just my way of checking if the pages were rendering correctly.
@@ -36,6 +36,7 @@ const Navbar = () => {
             <div className="links">
               <Link to="/login">Login</Link>
               <Link to="/signup">Signup</Link>
+              <Link to="/">{machine}</Link>
             </div>
           )}
         </nav>
