c6a18b0b8ccedf49717896ab8a35ace51714ec0e
Nicole Torres <nicolejoan9@yahoo.com>
2023-12-11 20:14:39 -0500

    Nicole - commented out font awesome

--- client/src/components/Square.jsx
+++ client/src/components/Square.jsx
@@ -1,17 +1,17 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { faCircle } from '@fortawesome/free-regular-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faTimes } from '@fortawesome/free-solid-svg-icons';
+// import { faCircle } from '@fortawesome/free-regular-svg-icons';
 
 function Square({ value, onClick }) {
     let icon;
 
-    if (value === 'X') {
-        icon = <FontAwesomeIcon icon={faTimes} size="2xl"style={{ color: "#ffc17a" }} />;
-    } else if (value === 'O') {
-        icon = <FontAwesomeIcon icon={faCircle} size="2xl" style={{ color: "#bc70d2" }} />;
-    } else {
+    // if (value === 'X') {
+    //     icon = <FontAwesomeIcon icon={faTimes} size="2xl"style={{ color: "#ffc17a" }} />;
+    // } else if (value === 'O') {
+    //     icon = <FontAwesomeIcon icon={faCircle} size="2xl" style={{ color: "#bc70d2" }} />;
+    // } else {
         icon = value;
-    }
+    // }
 
     return (
         <button className="square" onClick={onClick}>
