f94c586f29173a114467a7b0dcff7f3accefe46f
Nicole Torres <nicolejoan9@yahoo.com>
2023-12-11 22:12:08 -0500

    Nicole - fixed capital

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -17,7 +17,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../Navigation/Navbar";
-import "./header.css";
+import "./Header.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
