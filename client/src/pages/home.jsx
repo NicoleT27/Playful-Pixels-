471fbf2fe66f0e9234c84b111d9cf90bc4290e67
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-12-14 16:18:58 -0500

    we all worked on styling

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -13,7 +13,7 @@ function home() {
     <div className="flex-container">
       {/* first card  */}
       <div>
-        <Card className='card1' style={{ width: "18rem", border:"2px solid #80E4FF" }}>
+        <Card className='card1' style={{ width: "18rem", border:"2px solid #80E4FF", "background-color": "inherit"}}>
           <Card.Img variant="top" src={memGame} />
           <Card.Body style={{ padding: "0em" }}>
             <Card.Title className="gameTitle">Memory Game</Card.Title>
@@ -29,7 +29,7 @@ function home() {
       
       {/* second card  */}
       <div>
-        <Card className='card2' style={{ width: "18rem", border:"2px solid #80E4FF" }}>
+        <Card className='card2' style={{ width: "18rem", border:"2px solid #80E4FF", "background-color" : "inherit" }}>
           <Card.Img variant="top" src={candyCrush} />
           <Card.Body style={{ padding: "0em"}}>
             <Card.Title className="gameTitle">Candy Crush</Card.Title>
@@ -44,7 +44,7 @@ function home() {
       </div>
       {/* third card  */}
       <div>
-        <Card className='card3' style={{ width: "18rem", border:"2px solid #80E4FF" }}>
+        <Card className='card3' style={{ width: "18rem", border:"2px solid #80E4FF", "background-color" : "inherit" }}>
           <Card.Img variant="top" src={ticTac} />
           <Card.Body style={{ padding: "0em" }}>
             <Card.Title className="gameTitle">Tic Tac Toe</Card.Title>
